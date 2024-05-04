6f88944f1520fc5ea236602c5cc5cf08b463486c
ajsdklf <140897315+ajsdklf@users.noreply.github.com>
2024-05-04 11:46:02 +0900

    settings

---
+++ main.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+import streamlit as st 
+import numpy as np 
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=OPENAI_API_KEY)
+
