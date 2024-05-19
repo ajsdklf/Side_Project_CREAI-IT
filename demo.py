93e1d7af8e8c3f6a8d808195b0b794da02e4076b
ajsdklf <140897315+ajsdklf@users.noreply.github.com>
2024-05-19 12:11:43 +0900

    sd

--- demo.py
+++ demo.py
@@ -19,6 +19,8 @@ import numpy as np
 from dotenv import load_dotenv
 import os 
 from pathlib import Path
+from llama_index.core import PromptTemplate
+
 
 st.header('영유아 선생님 도우미')
 
@@ -39,15 +41,26 @@ You have to summerize the user's input into two sentences.
 Note that the cause of the sypmtom is one of the followings; ADHD, Autism, Seperation anxiety disorder.
 """
 
-new_prompt_qa = """
-Context information is below.
----------------------
+FINAL_ANSWER_PROMPT = """
+As an [Experience Kindergarten Teacher], you are tasked with suggesting [3 steps] of [code of conduct] for the user based on their symptoms. The response should provide a structured plan in 3 steps for the  kindergarten teachers who are in need of help when encountered with children who have problem behavior.
+
+Your answer should be in the form instructed:
+-------------
+[Form of Answer]
+[Step1] : [Things user should do first to mitigate children's misbehavior.]
+[Step2] : [Thinkgs user should do after conductiong step1.]
+[Step3] : [Final steps user should take to fully mitigate toddler's misbehavior.]
+-------------
+
+The child’s misbehavior you have to consult right now is the following:
+-------------
+[Misbehavior of child]
+{query_str}
+
+When providing a consulting, take into account the following context. Following context includes information related to children’s current misbehavior. Context is the following:
+[Context information]
 {context_str}
----------------------
-Given the context information and not prior knowledge, answer the query.
-Use proper Korean, and answer in the style of kindergarten teacher, gentle and enthusiastic.
-Query: {query_str}
-Answer:
+-------------
 """
 
 new_prompt_refine = """
@@ -83,21 +96,26 @@ if option == 'ASD':
         
         nodes = retriever.retrieve(summarized_answer)
         
-        response_synthesizer = get_response_synthesizer(response_mode='compact')
-        
-        response = response_synthesizer.synthesize(
-            summarized_answer, 
-            nodes=retriever.retrieve(summarized_answer)
-        )
-        
         query_engine = vector_index.as_query_engine()
         prompts_dict = query_engine.get_prompts()
-
+        
+        FINAL_ANSWER_PROMPT = PromptTemplate(FINAL_ANSWER_PROMPT)
+        new_prompt_refine = PromptTemplate(new_prompt_refine)
+        
+        query_engine.update_prompts(
+            {"response_synthesizer:text_qa_template": FINAL_ANSWER_PROMPT, "response_synthesizer:refine_template": new_prompt_refine}
+            )
+        
+        response_synthesizer = get_response_synthesizer(response_mode='compact')
+        
+        response = query_engine.query(summarized_answer)
+        
         with st.expander('조언 생성이 완료됐습니다!'):
             st.markdown(f"👨‍⚕️최종적인 조언👨‍⚕️: {response.response}")
             for node in nodes:
                 st.markdown(f"{node.text}란 문장이랑 유사한 행동을 보이네요!")
             st.markdown(summarized_answer)
+            st.markdown(query_engine.get_prompts())
 
 if option == 'ADHD':
     storage_context = StorageContext.from_defaults(persist_dir="~/dataset/CREAI+IT_side_project/adhd_index")
@@ -122,13 +140,17 @@ if option == 'ADHD':
         
         response_synthesizer = get_response_synthesizer(response_mode='compact')
         
-        response = response_synthesizer.synthesize(
-            summarized_answer, 
-            nodes=retriever.retrieve(summarized_answer)
-        )
-        
         query_engine = vector_index.as_query_engine()
         prompts_dict = query_engine.get_prompts()
+        
+        FINAL_ANSWER_PROMPT = PromptTemplate(FINAL_ANSWER_PROMPT)
+        new_prompt_refine = PromptTemplate(new_prompt_refine)
+        
+        query_engine.update_prompts(
+            {"response_synthesizer:text_qa_template": FINAL_ANSWER_PROMPT, "response_synthesizer:refine_template": new_prompt_refine}
+            )
+        
+        response = query_engine.query(summarized_answer)
 
         with st.expander('조언 생성이 완료됐습니다!'):
             st.markdown(f"👨‍⚕️최종적인 조언👨‍⚕️: {response.response}")
@@ -159,13 +181,17 @@ if option == 'SDA':
         
         response_synthesizer = get_response_synthesizer(response_mode='compact')
         
-        response = response_synthesizer.synthesize(
-            summarized_answer, 
-            nodes=retriever.retrieve(summarized_answer)
-        )
+        FINAL_ANSWER_PROMPT = PromptTemplate(FINAL_ANSWER_PROMPT)
+        new_prompt_refine = PromptTemplate(new_prompt_refine)
+        
+        query_engine.update_prompts(
+            {"response_synthesizer:text_qa_template": FINAL_ANSWER_PROMPT, "response_synthesizer:refine_template": new_prompt_refine}
+            )
         
         query_engine = vector_index.as_query_engine()
         prompts_dict = query_engine.get_prompts()
+        
+        response = query_engine.query(summarized_answer)
 
         with st.expander('조언 생성이 완료됐습니다!'):
             st.markdown(f"👨‍⚕️최종적인 조언👨‍⚕️: {response.response}")
